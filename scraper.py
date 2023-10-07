05ee73bf8ee4ed9e93ec91db15dd201779c2bfc2
Isaiah <124836428+w1ggle@users.noreply.github.com>
2023-10-07 12:19:53 -0400

    getting links and htmls

--- scraper.py
+++ scraper.py
@@ -2,3 +2,17 @@ import setup
 from bs4 import BeautifulSoup
 import requests
 import csv
+
+
+mainLandingURL = "http://ruoncampus.rutgers.edu/facilities/browse-by-campus/"
+
+buschURL = "http://ruoncampus.rutgers.edu/facilities/browse-by-campus/explore-rutgers-campus-housing-facilities-busch-campus/"
+
+allenHallURL = "http://ruoncampus.rutgers.edu/allen-hall/"
+
+#getting htmls
+print("Scraping URLs")  #TODO add if statement to check if we got a request, else print error
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/99.0", 
+    "method": "GET"
+}
