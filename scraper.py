6ac060bca35dcf10e478b7362f88224e15462d52
Isaiah <124836428+w1ggle@users.noreply.github.com>
2023-10-07 12:25:04 -0400

    add ignore

--- scraper.py
+++ scraper.py
@@ -11,8 +11,19 @@ buschURL = "http://ruoncampus.rutgers.edu/facilities/browse-by-campus/explore-ru
 allenHallURL = "http://ruoncampus.rutgers.edu/allen-hall/"
 
 #getting htmls
-print("Scraping URLs")  #TODO add if statement to check if we got a request, else print error
+print("Scraping URLs")  
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/99.0", 
     "method": "GET"
 }
+
+page_to_scrape = requests.get(allenHallURL,headers=headers) 
+residenceSoup = BeautifulSoup(page_to_scrape.text, 'html.parser') 
+
+file = open('output.csv', 'w')
+writer = csv.writer(file)
+writer.writerow(['Type', 'Max Students', '# of Floors', 'Avg Room Size', 'Availability', 'Contract Type']) #create CSV file
+
+info = residenceSoup.findAll('div', attrs={"class":"pb_wrapper"})[6]
+
+print(info)
