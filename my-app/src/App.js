1a24e170146048b3072db437b4eb5884cea85195
David <davidhariantowu@gmail.com>
2023-10-08 10:06:39 -0400

    Fetch checkpoint

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,19 +4,42 @@ import { Survey } from 'survey-react-ui';
 import { useCallback, useState } from 'react';
 import MainPage from './MainPage';
 import { surveyJson } from './surveyJson';
+import axios from 'axios';
 
 function App() {
   const [surveyResults, setSurveyResults] = useState(null);
   const [formStarted, setFormStarted] = useState(false)
+  const [dormData, setDormData] = useState(null);
   const survey = new Model(surveyJson);
 
   const onComplete = useCallback((sender) => {
     setSurveyResults(JSON.stringify(sender.data));
+    fetchDorms(surveyResults);
   }, []);
 
   const handleStartForm = () => {
     setFormStarted(true)
   }
+
+  const fetchDorms =  ((surveyResults) => {     //failed to fetch, is this the correct way to fetch
+    fetch("http://localhost:1234/dorm-result", { //if so, why are we getting errors (CORS maybe? idk what it is)
+      method: "POST",                             //fetch is getting response of http://localhost:1234/dorm-result
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(surveyResults),
+    }).then((response) => {
+      console.log(response)
+    })
+  })
+        
+    //)
+    //.catch(error => {
+    //  console.error('Error retrieving data:', error);
+    //});    
+    //const data = await response.json(); 
+    //console.log(data)
+
 
   survey.onComplete.add(onComplete);
   return (
@@ -25,7 +48,7 @@ function App() {
         surveyResults ? (
         <div>
           <h2>Survey Results:</h2>
-          <p>{surveyResults}</p>
+          {/* <p>{dormData}</p> */}
         </div>
       ) : (
         <div>
